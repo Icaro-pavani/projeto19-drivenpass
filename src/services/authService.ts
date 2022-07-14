3de4acb807b993ea45e2445a8f115645787719d0
Icaro-pavani <icaro.pavani79@gmail.com>
2022-07-14 11:35:07 -0300

    fix: register user route

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,10 +4,14 @@ import { conflictError } from "../middlewares/handleErrorsMiddleware.js";
 import * as userRepository from "../repositories/userRepository.js";
 
 export async function addNewUser(userInfo: userRepository.CreateUserData) {
+  const SALT = 10;
   const userRegistred = await userRepository.findByEmail(userInfo.email);
   if (!!userRegistred) {
     throw conflictError("Email already registred!");
   }
+
+  const hashPassword = bcrypt.hashSync(userInfo.password, SALT);
+  userInfo.password = hashPassword;
 
   await userRepository.insert(userInfo);
 }
