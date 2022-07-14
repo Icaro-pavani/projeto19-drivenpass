34bd5835d3e9fdc5db6437e647d084a25cf311a6
Icaro-pavani <icaro.pavani79@gmail.com>
2022-07-14 11:39:03 -0300

    fix: minimum password length verification

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -3,7 +3,7 @@ import { CreateUserData } from "../repositories/userRepository.js";
 
 const userSchema = Joi.object<CreateUserData>({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(10).required(),
 });
 
 export default userSchema;
